2a9f93d1fa377b90633efc7c0fdce1e4c50c1311
sophie <sosophiel@hotmail.com>
2023-02-07 01:40:21 +0200

    Week4-Day2-DC

---
+++ Week4/Day2/DailyChallenge/script.js
@@ -0,0 +1,51 @@
+
+let client = "John";
+
+const groceries = {
+    fruits : ["pear", "apple", "banana"],
+    vegetables: ["tomatoes", "cucumber", "salad"],
+    totalPrice : "20$",
+    other : {
+        payed : true,
+        meansOfPayment : ["cash", "creditCard"]
+    }
+}
+
+displayGroceries = () => { 
+   groceries.fruits.forEach(fruit => console.log(fruit));
+}
+
+displayGroceries()
+
+cloneGroceries = () => {
+   let user = client
+
+   client = "Betty"
+   console.log(client, user)
+
+   let shopping = groceries
+   groceries.totalPrice = "35$"
+   console.log(groceries, shopping)
+
+   groceries.other.payed = false
+   console.log(groceries, shopping)
+
+}
+
+cloneGroceries()
+/**
+ * Hint: To do this daily challenge, take a look at today’s lesson Pass By Value & Pass By Reference
+
+Create an arrow function named displayGroceries, that console.logs the 3 fruits from the groceries object. Use the forEach method. 
+
+Create another arrow function named cloneGroceries.
+In the function, create a variable named user that is a copy of the client variable. 
+(Tip : make the user variable equal to the client variable)
+Change the client variable to “Betty”. Will we also see this modification in the user variable ? Why ?
+In the function, create a variable named shopping that is equal to the groceries variable.
+Change the value of the totalPrice key to 35$. Will we also see this modification in the shopping object ? Why ?
+Change the value of the payed key to false. Will we also see this modification in the shopping object ? Why ?
+
+Invoke the cloneGroceries function.
+
+ */
