7958ad74c536a032a8138d087d86c38fa6730152
sophie <sosophiel@hotmail.com>
2023-02-13 09:21:37 +0200

    W5-D1-DC-XP

---
+++ Week5/Day1/ExercisesXP/ExercisesXP-2/script.js
@@ -0,0 +1,9 @@
+/* Use the same form structure as Exercise 1.
+When you click the Send button, the form will be submitted with a POST method.(you can use the same HTML file for the action url)
+Where will the sent data appear? Hint : Look at the Network Tab
+ 
+
+the data won't appear in the url because the method is POST and won't allowed to be called by the browser
+http://127.0.0.1:5501/Week5/Day1/ExercisesXP/ExercisesXP-1/index.html
+
+*/
